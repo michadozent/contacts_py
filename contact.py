fdb768359fe0e3118aab8c11a357c78af4748b1c
micha schirmer <micha.schirmer@gmail.com>
2024-12-11 22:06:04 +0100

    Kommentare hinzugefügt

--- contact.py
+++ contact.py
@@ -1,16 +1,19 @@
 contacts={} # dictionary
 
-
+''' add Contact to Contacts'''
 def add_contact(name,number):
     contacts[name]=number      # Element hinzufügen [key]=value (appen)
 
+''' print all contacts'''
 def show_all():
     for c in contacts:
         print(c, contacts[c]) # key: value -> Max:01773423423
 
+'''find Number from Name'''
 def find(name):
     return contacts[name]
 
+'''give a new Number'''
 def upate_contact(name,number):
     contacts[name]=number      # der vorhandene Name (Key) wird überschrieben -> key ist immer eindeutig (nur einmal)
 
